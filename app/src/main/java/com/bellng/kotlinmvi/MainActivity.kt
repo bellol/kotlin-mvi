334f0d0ddb0ea8145f2533c2303bc223985600a8
Bell Ng <bell.ng@alumni.monash.edu>
2017-05-29 23:37:47 +1000

    added kotlin extentions

--- app/src/main/java/com/bellng/kotlinmvi/MainActivity.kt
+++ app/src/main/java/com/bellng/kotlinmvi/MainActivity.kt
@@ -3,22 +3,12 @@ package com.bellng.kotlinmvi
 import android.graphics.Typeface
 import android.os.Bundle
 import android.view.View
-import android.widget.Button
-import android.widget.TextView
-import butterknife.BindView
-import butterknife.ButterKnife
 import com.hannesdorfmann.mosby3.mvi.MviActivity
 import com.jakewharton.rxbinding2.view.clicks
 import io.reactivex.Observable
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : MviActivity<MainView, MainPresenter>(), MainView {
-
-    @BindView(R.id.text) lateinit var text: TextView
-    @BindView(R.id.increment_button) lateinit var incrementButton: Button
-    @BindView(R.id.decrement_button) lateinit var decrementButton: Button
-    @BindView(R.id.bold_on_button) lateinit var boldOnButton: Button
-    @BindView(R.id.bold_off_button) lateinit var boldOffButton: Button
-    @BindView(R.id.reset_button) lateinit var resetButton: Button
 
     override fun createPresenter(): MainPresenter {
         return MainPresenter()
@@ -27,31 +17,28 @@ class MainActivity : MviActivity<MainView, MainPresenter>(), MainView {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        ButterKnife.bind(this)
     }
 
     override fun render(viewState: MainViewState) {
-        if (viewState.hasBoldText) {
-            text.setTypeface(null, Typeface.BOLD)
-        } else {
-            text.setTypeface(null, Typeface.NORMAL)
+        when (viewState.hasBoldText) {
+            true -> text_view.setTypeface(null, Typeface.BOLD)
+            else -> text_view.setTypeface(null, Typeface.NORMAL)
         }
 
-        if (viewState.showResetButton) {
-            resetButton.visibility = View.VISIBLE
-        } else {
-            resetButton.visibility = View.GONE
+        when (viewState.showResetButton) {
+            true -> reset_button.visibility = View.VISIBLE
+            else -> reset_button.visibility = View.GONE
         }
 
-        text.text = viewState.count.toString()
+        text_view.text = viewState.count.toString()
     }
 
-    override fun incrementIntent() = incrementButton.clicks()
+    override fun incrementIntent() = increment_button.clicks()
 
-    override fun decrementIntent() = decrementButton.clicks()
+    override fun decrementIntent() = decrement_button.clicks()
 
-    override fun resetCounterIntent() = resetButton.clicks()
+    override fun resetCounterIntent() = reset_button.clicks()
 
-    override fun toggleBoldTextIntent(): Observable<Boolean> = Observable.merge(boldOnButton.clicks().map { true }, boldOffButton.clicks().map { false })
+    override fun toggleBoldTextIntent(): Observable<Boolean> = Observable.merge(bold_on_button.clicks().map { true }, bold_off_button.clicks().map { false })
 
 }
